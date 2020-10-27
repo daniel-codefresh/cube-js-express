87f3988c2750baf1ced4188c6517e53c8bbbe2bc
daniel-codefresh <daniel.soifer@codefresh.io>
2020-10-27 21:32:47 +0200

    overall improved logic

--- sourceEntities/BigQuery.js
+++ sourceEntities/BigQuery.js
@@ -1,10 +1,20 @@
-const SourceEntity = require('./SourceEntity');
 const BqDriver = require('@cubejs-backend/bigquery-driver');
-const cubeEntities = require('../bqCubeEntities');
+const SourceEntity = require('./SourceEntity');
+const { Account, Build, Concurrency } = require('../CubeEntities');
+const constants = require('../cubeEntitiyNames');
+const accountSchema = require('../bqSchemas/accountSchema');
+const buildSchema = require('../bqSchemas/buildSchema');
+const concurrencySchema = require('../bqSchemas/concurrencySchema');
+
+const bqCubeEntities = {
+    [constants.ACCOUNT]: new Account(accountSchema, constants.ACCOUNT),
+    [constants.BUILD]: new Build(buildSchema, constants.BUILD),
+    [constants.CONCURRENCY]: new Concurrency(concurrencySchema, constants.CONCURRENCY)
+};
 
 class BigQuery extends SourceEntity {
     constructor() {
-        super(BqDriver, cubeEntities);
+        super(BqDriver, bqCubeEntities);
     }
 }
 
